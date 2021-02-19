78771384a30b046465c79c260de38ce5ef15584d
roy yoo <royyoo123@gmail.com>
2021-02-19 14:12:55 -0800

    VERSION 1.0

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -21,7 +21,7 @@ class CocktailsController < ApplicationController
 
 	private 
 	def cocktail_params
-    params.require(:cocktail).permit(:name, :photo)
+    params.require(:cocktail).permit(:name, photos: [])
   end
 
 end
