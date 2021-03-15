f73eb4e0a21b696182667af0c43cb9dc37029fcb
roy yoo <royyoo123@gmail.com>
2021-03-15 16:16:37 -0700

    changed params order

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -21,7 +21,7 @@ class CocktailsController < ApplicationController
 
 	private 
 	def cocktail_params
-    params.require(:cocktail).permit(:name, :recipe,photos: [])
+    params.require(:cocktail).permit(:name, photos: [],:recipe)
   end
 
 end
